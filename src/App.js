d81c703f13279cdecb394e725ae7ffdc456c9c3b
eliascmendhes <eliascamilo70@outlook.com>
2022-12-23 17:02:07 -0300

    site cat

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    catUrl: "",
+  };
+
+  async fetchCat() {
+    const response = await fetch("https://api.thecatapi.com/v1/images/search");
+    const data = await response.json();
+    this.setState({ catUrl: data[0].url });
+  }
+
+  render() {
+    return (
+      <div>
+        <img className="cat-image" src={this.state.catUrl} alt="cat" />
+        <button className="button" onClick={() => this.fetchCat()}>
+          Próxima imagem
+        </button>
+      </div>
+    );
+  }
 }
 
 export default App;
